709e9d1c503645c2bfa29723849a744580462b2b
erennlk <elik.dark91@icloud.com>
2021-05-18 10:02:55 +0300

    Initial Commit

---
+++ TKUIViewRepresentable/TKUIViewRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TKUIViewRepresentableApp.swift
+//  TKUIViewRepresentable
+//
+//  Created by Eren  Çelik on 18.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct TKUIViewRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
